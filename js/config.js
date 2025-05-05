8e5016423d9ddd28bd0703598af8325368a5dc71
soumyasaswat09 <soumyasaswat09@gmail.com>
2025-05-04 23:28:34 -0500

    Update Cognito config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Mv3wiwia8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'arn:aws:cognito-idp:us-east-1:266735826442:userpool/us-east-1_Mv3wiwia8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Mv3wiwia8/.well-known/jwks.json' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
